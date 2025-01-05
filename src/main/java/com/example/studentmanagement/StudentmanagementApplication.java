6243c6dd716d224b63dfca5180fb69d72ad9648e
santhoshkumar07112000 <santhoshkumar07112000@gmail.com>
2025-01-05 12:42:35 +0530

    successfully started a application in 8471

--- src/main/java/com/example/studentmanagement/StudentmanagementApplication.java
+++ src/main/java/com/example/studentmanagement/StudentmanagementApplication.java
@@ -2,12 +2,23 @@ package com.example.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentmanagementApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementApplication.class, args);
+		System.out.println("*******************************");
+        System.out.println("server running on the port 8471");
+		System.out.println("*******************************");
 	}
 
+	@GetMapping("/")
+	public String Welcome(){
+		return "welcome to our student management applicartion running on the port 8471";
+	}
 }
